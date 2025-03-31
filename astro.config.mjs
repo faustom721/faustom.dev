d480f56d14cd4e4874f6cf92149385a963e5edde
Fausto Márquez <fausto_7m@hotmail.com>
2025-03-31 10:37:48 -0300

    Add GitHub Actions workflow for deploying to GitHub Pages; remove old static deployment workflow and update configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://faustom.dev'
-  integrations: [tailwind()]
+  site: 'https://faustom.dev',
+  integrations: [tailwind()],
 });
